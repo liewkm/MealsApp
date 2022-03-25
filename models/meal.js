1ee71933093d51e53d0c43049c17819b367e3113
liewkm <94386756+liewkm@users.noreply.github.com>
2022-03-26 00:57:20 +0800

    Initiated MealsApp

---
+++ models/meal.js
@@ -0,0 +1,21 @@
+class Meal (
+    constructor (
+        id,
+        categoryIds,
+        title,
+        affordability,
+        complexity,
+        imageUrl,
+        duration,
+        ingredients,
+        steps,
+        isGlutenFree,
+        isVegan,
+        isVegetarian,
+        isLactoseFree
+    ) {
+
+    }
+)
+
+export default Meal;
