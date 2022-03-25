e2e61bf228a8a1b98ed4a4f00eac6b620c19fdf7
liewkm <94386756+liewkm@users.noreply.github.com>
2022-03-26 01:23:38 +0800

    Update dir models, WIP on data dir.

---
+++ data/dummy-data.js
@@ -0,0 +1,11 @@
+import Category from '../models/category';
+import Meal from '../models/meal';
+
+export default CATEGORIES = [
+    new Category('c1', 'Italian', '#f5428d'),
+    new Category('c2', 'Quick & Easy', '#f54242'),
+
+
+
+
+]
--- models/meal.js
+++ models/meal.js
@@ -15,9 +15,19 @@ class Meal {
         isLactoseFree
      ) {
         this.id = id;
-
+        this.categoryIds = categoryIds;
+        this.title = title;
+        this.affordability = affordability;
+        this.complexity = complexity;
+        this.imageUrl = imageUrl;
+        this.duration = duration;
+        this.ingredients = ingredients;
+        this.steps = steps;
+        this.isGlutenFree = isGlutenFree;
+        this.isVegan = isVegan;
+        this.isVegetarian = isVegetarian;
+        this.isLactoseFree = isLactoseFree;
     }
 }
-
 
 export default Meal;
